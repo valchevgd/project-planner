90738c42d9c315d4cd80070eb5ca2a3deedc3dfd
Georgi Valchev <gvalchev@neterra.net>
2021-12-20 22:58:03 +0200

    Added: fetching and listing projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <p>Loading projects...</p>
+    </div>
   </div>
 </template>
 
@@ -8,5 +15,16 @@
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+        .then(res => res.json())
+        .then(data => this.projects = data)
+        .catch(err => console.log(err.message))
+  }
 }
 </script>
