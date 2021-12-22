c7fb8eb1966ecc97e1cb94ad5a1b0658119e1d3a
Georgi Valchev <gvalchev@neterra.net>
2021-12-22 09:02:32 +0200

    Added: Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
+  <Navbar />
   <router-view/>
 </template>
+<script>
+import Navbar from './components/Navbar';
 
+export default {
+  components: {Navbar}
+}
+</script>
 <style>
 body {
   background: #f2f2f2;
